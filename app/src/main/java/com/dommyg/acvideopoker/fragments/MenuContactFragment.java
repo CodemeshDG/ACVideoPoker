935a36e093eadc21fe6524b82ef7cb75edb35c09
CodemeshDG <DomG1984@hotmail.com>
2021-01-31 20:16:12 -0500

    Fixed bug about contact email subject line

--- app/src/main/java/com/dommyg/acvideopoker/fragments/MenuContactFragment.java
+++ app/src/main/java/com/dommyg/acvideopoker/fragments/MenuContactFragment.java
@@ -1,8 +1,10 @@
 package com.dommyg.acvideopoker.fragments;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -21,13 +23,25 @@ public class MenuContactFragment extends BaseFragment {
     }
 
     public void openEmail() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.putExtra(Intent.EXTRA_SUBJECT,
-                "Feedback for AC Video Poker");
-        intent.setData(Uri.parse("mailto: ACVideoPoker@gmail.com"));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent selectorIntent = new Intent(Intent.ACTION_SENDTO);
+        selectorIntent.setData(Uri.parse("mailto:"));
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"ACVideoPoker@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for AC Video Poker");
         // Flag for moving backwards to this app, not email app.
-        requireContext().startActivity(intent);
+        emailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        emailIntent.setSelector(selectorIntent);
+
+        try {
+            requireContext().startActivity(Intent.createChooser(emailIntent, null));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(
+                    requireContext(),
+                    R.string.error_activity_not_found,
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
     }
 
     @Override
